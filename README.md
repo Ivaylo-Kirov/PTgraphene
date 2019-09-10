13d4380086bfdaf67437bec77828bdb9beb0e02e
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-09-10 15:25:38 -0400

    Round 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 similar to how JS functions by associating objects with TYPES that graphene provides > seems like these types allow the GraphQL engine to treat these objects as required (i.e. ObjectType, Schema, String, Int) > then create a python class and variables and have resolve_variable methods to tell the engine what to return for those queries > schema.execute(query) to test it out
+
+* Round 2 - added another item to RootQuery and noticed that after executing a query, the returned object is a dict (i.e. ['about', 'About what']) and so when you print you do print(result['about'])
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-from graphene import ObjectType, String, Schema
+import graphene 
 
-class RootQuery(ObjectType):
-    hello = String()
-
+class RootQuery(graphene.ObjectType):
+    hello = graphene.String()
+    about = graphene.String()
     def resolve_hello(self, info):
         return 'World'
+    def resolve_about(self, info):
+        return 'About what'
 
-schema = Schema(query=RootQuery)
+schema = graphene.Schema(query=RootQuery)
 
 if __name__ == "__main__":
     print('running')
     result = schema.execute('{ hello }')
     print(result.data['hello'])
+    about_result = schema.execute('{ about }')
+    print(about_result.data['about'])
